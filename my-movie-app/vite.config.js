53bc422e60ce13d2955ecff0dba918f0ed27ab4a
anasabinomonteiro <mon23034@byui.edu>
2025-02-17 12:03:48 -0300

    Adjusts to structure paths on Vite

--- my-movie-app/vite.config.js
+++ my-movie-app/vite.config.js
@@ -1,7 +1,18 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/wdd330_my-movie-app/', // For Github pages
-});
+    root: 'src/',
 
-//     base: '', // Empty for Netlify
+    build: {
+        outDir: '../dist', // folder wdd330_my-movie-app/dist
+        emptyOutDir: true, // clean the dist folder before building
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                favorites: resolve(__dirname, './src/pages/favorites.html'),
+                where_watch: resolve(__dirname, './src/pages/where_watch.html'),
+            },
+        },
+    },
+});
