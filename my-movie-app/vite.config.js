449e6e290e763f204878299310ff4971b347c35f
anasabinomonteiro <mon23034@byui.edu>
2025-02-17 12:35:49 -0300

    Move public out of src and vite.config adjust- to solve error 404 on netlify when redirect page

--- my-movie-app/vite.config.js
+++ my-movie-app/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     root: 'src/',
+    publicDir: '../public', // folder wdd330_my-movie-app/public
 
     build: {
         outDir: '../dist', // folder wdd330_my-movie-app/dist
