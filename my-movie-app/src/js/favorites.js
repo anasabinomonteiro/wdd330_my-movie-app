ded0b191fb81d68ba9b69572702d050d663a7a85
anasabinomonteiro <mon23034@byui.edu>
2025-02-17 14:56:05 -0300

    Fixing loading of favorites.js_part2

--- my-movie-app/src/js/favorites.js
+++ my-movie-app/src/js/favorites.js
@@ -67,5 +67,5 @@ export function displayFavorites() {
 
 // Run if the favorites page is loaded
 if (window.location.pathname.includes('favorites.html')) {
-    document.addEventListener('load', displayFavorites);
+    document.addEventListener('DOMContentLoaded', displayFavorites);
 }
